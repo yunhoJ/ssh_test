d1a4f0e5a5c4cdd5f1c6fa6ca4c228dd73b1ee8d
admin <admin>
2022-02-09 09:16:20 +0000

    Saved recipe: TEST1.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -8,13 +8,25 @@ profile_test_prepared_distinct = dataiku.Dataset("profile_test_prepared_distinct
 profile_test_prepared_distinct_df = profile_test_prepared_distinct.get_dataframe()
 
 
+import requests
+from bs4 import BeautifulSoup as bs
+url=profile_test_prepared_distinct_df["Source URL"][0]
+# url=profile_prepared_distinct_df["Source URL"]
+res=requests.get(url)
+html=res.text
+soup=bs(html,"html.parser")
+text=soup.select("div.css-901oao")
+
+lis=[]
+lis.append(text)
+
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
 test_df = profile_test_prepared_distinct_df # For this sample code, simply copy input to output
 
-clwo2=pd.read_csv("/home/sam/dataiku_design/dss_data/jupyter-run/dku-workdirs/TEST/notebook_editor_for_compute_finalcc1adc65/crawl.csv",encoding="utf-8")
+test=pd.read_csv("/home/sam/dataiku_design/dss_data/jupyter-run/dku-workdirs/TEST/notebook_editor_for_compute_finalcc1adc65/crawl.csv",encoding="utf-8")
 # Write recipe outputs
-test = dataiku.Dataset("test")
-test.write_with_schema(clwo2)
+test_df = dataiku.Dataset("test")
+test.write_with_schema(test_df)
